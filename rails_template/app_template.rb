9846903bd9b16b0e330d65d184420e7f7de25f04
Edward Loveall <edward@edwardloveall.com>
2014-01-17 14:51:37 -0500

    Added rails template

---
+++ rails_template/app_template.rb
@@ -0,0 +1,42 @@
+ruby_comment = /^(\s)*#.*\n/
+
+# Gems
+gem 'bourbon'
+gem 'neat'
+gem 'simple_form'
+gem 'quiet_assets'
+
+# Modify the database config
+gsub_file 'config/database.yml', /username\: .+/, "username: <% ENV['POSTGRES_USERNAME'] %>"
+gsub_file 'config/database.yml', ruby_comment, ''
+
+# Clear Routes
+gsub_file 'config/routes.rb', ruby_comment, ''
+
+# Gemfile
+gsub_file 'Gemfile', /gem 'coffee-rails'.*/, ''
+gsub_file 'Gemfile', ruby_comment, ''
+
+# Remove Assets
+remove_file 'app/assets/javascripts/application.js'
+remove_file 'app/assets/stylesheets/application.css'
+
+# Add Custom Assets
+template_dir = "#{File.dirname(__FILE__)}/template"
+files = []
+
+inside template_dir do
+  files = Dir.glob("**/*.*")
+end
+
+files.each do |file|
+  copy_file [template_dir, file].join('/'), file
+end
+
+# Create database
+run 'rake db:create'
+
+# Git Repo
+git :init
+git add: '.'
+git commit: '-m "Initial commit."'
