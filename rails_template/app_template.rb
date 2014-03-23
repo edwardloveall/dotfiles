204ad3e84ac66dbaac1658e3ed847c9e85cf5440
Edward Loveall <edward@edwardloveall.com>
2014-03-23 16:22:38 -0400

    Added better gemfile to template

--- rails_template/app_template.rb
+++ rails_template/app_template.rb
@@ -1,11 +1,5 @@
 ruby_comment = /^(\s)*#.*\n/
 
-# Gems
-gem 'bourbon'
-gem 'neat'
-gem 'simple_form'
-gem 'quiet_assets'
-
 # Modify the database config
 gsub_file 'config/database.yml', /username\: .+/, "username: <% ENV['POSTGRES_USERNAME'] %>"
 gsub_file 'config/database.yml', ruby_comment, ''
@@ -13,20 +7,20 @@ gsub_file 'config/database.yml', ruby_comment, ''
 # Clear Routes
 gsub_file 'config/routes.rb', ruby_comment, ''
 
-# Gemfile
-gsub_file 'Gemfile', /gem 'coffee-rails'.*/, ''
-gsub_file 'Gemfile', ruby_comment, ''
+# Clear Gemfile
+remove_file 'Gemfile'
 
 # Remove Assets
 remove_file 'app/assets/javascripts/application.js'
 remove_file 'app/assets/stylesheets/application.css'
 
-# Add Custom Assets
+# Add Custom Files
 template_dir = "#{File.dirname(__FILE__)}/template"
 files = []
 
 inside template_dir do
   files = Dir.glob("**/*.*")
+  files << 'Gemfile'
 end
 
 files.each do |file|
