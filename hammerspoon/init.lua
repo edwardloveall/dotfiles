26ac495762e9aebf4b25e421ee5dd9675eaa97dc
Edward Loveall <edward@edwardloveall.com>
2020-02-23 09:32:26 -0500

    Refactor and fixes

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,123 +1,122 @@
 iconWidth = 156
 hs.window.animationDuration = 0
-laptopScreen = "Color LCD"
-homeMainMonitor = "DELL U2715H"
+laptopScreenId = "Color LCD"
+homeMainScreenId = "iMac"
+
+primaryScreen = hs.screen.primaryScreen()
+laptopScreen = hs.screen.find(laptopScreenId)
+iMacOnly = primaryScreen:name() == homeMainScreenId
+
+if iMacOnly then
+  laptopScreen = primaryScreen
+end
 
 -- Add constant to layout
 function maximizedWithIcons()
-  local screen = hs.screen.primaryScreen()
-  local screenFrame = screen:frame()
+  local screenFrame = primaryScreen:frame()
   return hs.geometry.rect(0, 0, 1 - (iconWidth / screenFrame.w), 1)
 end
 hs.layout.maximizedWithIcons = maximizedWithIcons()
 
 -- Modal setup
-local modal = hs.hotkey.modal.new({"shift", "cmd"}, ";")
-local enterModal = function() modal:enter() end
-local exitModal = function() modal:exit() end
+modal = hs.hotkey.modal.new({"shift", "cmd"}, ";")
+enterModal = function() modal:enter() end
+exitModal = function() modal:exit() end
+
+-- Window Filters
+primaryFrame = primaryScreen:frame()
+smallWidth = primaryFrame.w * 0.40
+smallHeight = primaryFrame.h * 0.60
+allOther = hs.window.filter.new(
+  function(win)
+    local frame = win:frame()
+    return frame.w > smallWidth and frame.h > smallHeight
+  end
+)
+allOther:setFilters({
+  ["Mastonaut"] = false,
+  ["Messages"] = false,
+  ["Slack"] = false,
+  ["Gitter"] = false,
+  ["Hammerspoon"] = false,
+})
+allOtherWindows = function()
+  return allOther:getWindows()
+end
 
 -- Main window with a small strip on the right to see desktop icons
 modal:bind({}, "1", function()
   local win = hs.window:focusedWindow()
-  local screen = hs.screen.primaryScreen()
-  local frame = screen:fromUnitRect(hs.layout.maximizedWithIcons)
+  local frame = primaryScreen:fromUnitRect(hs.layout.maximizedWithIcons)
 
   win:setFrame(frame)
-  win:moveToScreen(homeMainMonitor, true)
+  win:moveToScreen(homeMainScreenId, true)
   exitModal()
 end)
 
 -- Thin window on the left (messages, mastodon, etc)
 modal:bind({}, "2", function()
   local win = hs.window:focusedWindow()
-  local screen = hs.screen.primaryScreen()
-  local frame = screen:fromUnitRect(hs.layout.left25)
+  local frame = primaryScreen:fromUnitRect(hs.layout.left25)
 
   win:setFrame(frame)
-  win:moveToScreen(homeMainMonitor, true)
-  exitModal()
-end)
-
-modal:bind({}, "[", function()
-  local win = hs.window:focusedWindow()
-  local screen = hs.screen.find(laptopScreen)
-  local frame = screen:fromUnitRect(hs.layout.maximized)
-
-  win:setFrame(frame)
-  win:moveToScreen(homeMainMonitor, true)
+  win:moveToScreen(homeMainScreenId, true)
   exitModal()
 end)
 
 -- Full size on laptop screen
 modal:bind({}, "[", function()
   local win = hs.window:focusedWindow()
-  local screen = hs.screen.find(laptopScreen)
-  local screenFrame = screen:frame()
+  local screenFrame = laptopScreen:frame()
   local widthWithIconSpace = screenFrame.w - iconWidth
   local frame = hs.geometry.rect(0, 0, widthWithIconSpace, screenFrame.h)
 
   win:setFrame(frame)
-  win:moveToScreen(laptopScreen, true)
+  win:moveToScreen(laptopScreenId, true)
   exitModal()
 end)
 
 modal:bind({}, "left", function()
   local win = hs.window:focusedWindow()
-  local screen = hs.screen.primaryScreen()
-  local frame = screen:fromUnitRect(hs.layout.left50)
+  local frame = primaryScreen:fromUnitRect(hs.layout.left50)
 
   win:setFrame(frame)
-  win:moveToScreen(homeMainMonitor, true)
+  win:moveToScreen(homeMainScreenId, true)
   exitModal()
 end)
 
 modal:bind({}, "right", function()
   local win = hs.window:focusedWindow()
-  local screen = hs.screen.primaryScreen()
-  local frame = screen:fromUnitRect(hs.layout.right50)
+  local frame = primaryScreen:fromUnitRect(hs.layout.right50)
 
   win:setFrame(frame)
-  win:moveToScreen(homeMainMonitor, true)
+  win:moveToScreen(homeMainScreenId, true)
   exitModal()
 end)
 
 modal:bind({}, "return", function()
-  local primaryMonitor = hs.screen.primaryScreen()
-  local mainFrame = primaryMonitor:frame()
-  local widthWithIconSpace = mainFrame.w - iconWidth
-  local fullWithIconSpace = hs.geometry.rect(0, 0, widthWithIconSpace, mainFrame.h)
-  if primaryMonitor:name() == laptopScreen then
-    local windowLayout = {
-      {nil, allWindowsFilter, primaryMonitor, nil, fullWithIconSpace, nil},
-      -- some specific app window configs
-      {"Gitter", nil, laptopScreen, hs.layout.maximized, nil, nil},
-      {"Messages", nil, primaryMonitor, hs.layout.left25, nil, nil},
-      {"Slack", nil, laptopScreen, hs.layout.maximized, nil, nil},
-      {"Terminal", nil, primaryMonitor, hs.layout.left50, nil, nil},
-    }
-    hs.layout.apply(windowLayout)
-  else
-    local windowLayout = {
-      {nil, allWindowsFilter, primaryMonitor, nil, fullWithIconSpace, nil},
-    }
-    hs.layout.apply(windowLayout)
+  local mainFrame = primaryScreen:frame()
+  local fullWithIconSpace = primaryScreen:fromUnitRect(hs.layout.maximizedWithIcons)
+  local laptopWidth = fullWithIconSpace
+
+  if iMacOnly then
+    local halfWidth = laptopScreen:fromUnitRect(hs.layout.left50)
+    laptopWidth = hs.geometry.rect(0, 0, halfWidth.w, mainFrame.h)
   end
+  print(table.unpack(allOtherWindows()))
+
+  local windowLayout = {
+    { nil, allOtherWindows, primaryScreen, nil, fullWithIconSpace, nil },
+    {"Messages", nil, primaryScreen, hs.layout.left25, nil, nil},
+    {"Mastonaut", nil, primaryScreen, hs.layout.left50, nil, nil},
+    {"Slack", nil, laptopScreen, nil, laptopWidth, nil},
+    {"Gitter", nil, laptopScreen, nil, laptopWidth, nil},
+  }
+
+  hs.layout.apply(windowLayout)
+
   exitModal()
 end)
-
--- filter for windows
-function allWindowsFilter(_)
-  local filter = hs.window.filter.default
-  filter:unsubscribeAll()
-  filter:rejectApp("Finder")
-  filter:rejectApp("Gitter")
-  filter:rejectApp("Hammerspoon")
-  filter:rejectApp("Messages")
-  filter:rejectApp("Slack")
-  filter:rejectApp("Terminal")
-
-  return filter:getWindows()
-end
 
 function reloadConfig(files)
     doReload = false
